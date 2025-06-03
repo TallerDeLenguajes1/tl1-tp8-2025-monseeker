d97c4786e19a3fafcfddecdb647b7686223a3f3d
FacundoPeraltaRomero <facundoperalta217@gmail.com>
2025-06-03 11:54:45 -0300

    Consigna inicial

---
+++ ToDo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Tarea {
+ public int TareaID { get; set; }
+ public string Descripcion { get; set; }
+ public int Duracion { get; set; } // Validar que esté entre 10 y 100
+ // Puedes añadir un constructor y métodos auxiliares si lo consideras necesario
+}
+
